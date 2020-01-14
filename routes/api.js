59db1d53fa01a257755529e3feddf139c1aadf5e
Glitch (shy-chiller) <none>
2020-01-14 19:23:01 +0000

    📙🏀 Checkpoint ./routes/api.js:9182067/452

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,24 @@
 'use strict';
 
 var expect = require('chai').expect;
+const MongoClient = require('mongodb').MongoClient;
+
+const connection = MongoClient.connect({
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const collection = client => {
+  return client.db("test").collection("messageBoard");
+}
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board').post((req, res) => {
+    connection.then(client => {
+      collection(client)
+    })
+  })
     
   app.route('/api/replies/:board');
 

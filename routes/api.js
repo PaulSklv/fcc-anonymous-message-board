d760e83e18c9142cccc5fca4869b672242025d07
Glitch (shy-chiller) <none>
2020-01-15 17:21:05 +0000

    🎍🚿 Checkpoint ./routes/api.js:9182067/4125

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,15 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-const { MongoClient, ObjectID } = require('mongodb');
+var expect = require("chai").expect;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connection = MongoClient.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -18,37 +18,67 @@ const connection = MongoClient.connect(process.env.MONGO_URI, {
 
 const collection = (client, req) => {
   return client.db("messageBoard").collection(req.params.board);
-}
+};
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board')
+module.exports = function(app) {
+  app
+    .route("/api/threads/:board")
     .post((req, res) => {
-    connection.then(client => {
-      collection(client, req).insertOne({
-        text: req.body.text,
-        delete_password: req.body.delete_password,
-        created_on: new Date(),
-        bumped_on: new Date(),
-        reported: false,
-        replies: []
-      }).then(result => {
-        res.redirect('/b/' + req.params.board);
-      }).catch(error => {return console.log("Something went wrong", error)})
+      connection.then(client => {
+        collection(client, req)
+          .insertOne({
+            text: req.body.text,
+            delete_password: req.body.delete_password,
+            created_on: new Date(),
+            bumped_on: new Date(),
+            reported: false,
+            replies: []
+          })
+          .then(result => {
+            res.redirect("/b/" + req.params.board);
+          })
+          .catch(error => {
+            return console.log("Something went wrong", error);
+          });
+      });
     })
-  })
-  .get((req, res) => {
-    connection.then(client => {
-      collection(client, req).find({ }).toArray().then(result => {
-        return res.send(result);
-      }).catch(error => {return console.log("Something went wrong", error)})
-    })
-  })
-    
-  app.route('/api/replies/:board').post((req, res) => {
-    connection.then(client => {
-      collection(client, req).updateOne({ _id: req.body.thread_id })
-    })
-  })
+    .get((req, res) => {
+      connection.then(client => {
+        collection(client, req)
+          .find({})
+          .toArray()
+          .then(result => {
+            return res.send(result);
+          })
+          .catch(error => {
+            return console.log("Something went wrong", error);
+          });
+      });
+    });
 
+  app.route("/api/replies/:board").post((req, res) => {
+    connection.then(client => {
+      console.log(req.body.thread_id)
+      collection(client, req).updateOne(
+        { _id: req.body.thread_id },
+        {
+          $push: {
+            replies: {
+              _id: new Object(),
+              text: req.body.text,
+              created_on: new Date(),
+              delete_passwor: req.body.delete_password,
+              reported: false
+            }
+          },
+          $set: {
+            $currentDate: { bumped_on: true }
+          }
+        }
+      ).then(result => {
+        console.log(result)
+        console.log(result.ops)
+      });
+    });
+  });
 };

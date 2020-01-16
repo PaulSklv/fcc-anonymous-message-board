5d919f49d1c7b7ea61a8b22b217bcc604f56b5a1
Glitch (shy-chiller) <none>
2020-01-16 18:50:05 +0000

    ⛵️💇 Checkpoint ./tests/2_functional-tests.js:9182067/1139

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -17,17 +17,37 @@ suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     
-    suite('POST', function() {
+    suite('POST', function(done) {
       chai
         .request(server)
         .post("/api/threads/board")
         .send({
-        
+          board: "board",
+          title: "Title",
+          delete_password: "test"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.equal(res.body.title, "Title");
+          assert.equal(res.body.delete_password, "test");
+          assert.equal(res.body.board, "test");
+          done();
       })
     });
     
-    suite('GET', function() {
-      
+    suite('GET', function(done) {
+      chai
+        .request(server)
+        .get("/api/threads/board")
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.equal(res.body.title, "Title");
+          assert.equal(res.body.delete_password, "test");
+          assert.equal(res.body.board, "test");
+          done();
+      })
     });
     
     suite('DELETE', function() {
